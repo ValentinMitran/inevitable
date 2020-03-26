8d6b4c4ee8c5e589aea6e393343e5486c960930b
Valentin Mitran <ToolsVM@pm.me>
2020-03-26 20:44:20 +0000

    The beginning of a new world

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
+//Import Routes
+const authRoute = require("./routes/auth");
+
+//Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("DB up n running")
+);
+
+//Middleware
+app.use(express.json());
+
+//Route Middlewares
+app.use("/api/user", authRoute);
+
+
+app.listen(process.env.PORT || 3001, () => console.log("Express running"));
---
+++ web/src/routes/Routes.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Routes() {
+  return <>Routes</>;
+}
+
+export default Routes;
