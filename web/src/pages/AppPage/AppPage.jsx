1d8f11b81e93fbe7a9328481bdda7d7cc0ac6bea
Valentin Mitran <ToolsVM@pm.me>
2020-04-10 00:44:14 +0100

    Quick css fixes

--- web/src/pages/AppPage/AppPage.jsx
+++ web/src/pages/AppPage/AppPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import Error404 from "./../ErrorPage/Error404";
 import LeftNav from "./../../components/Navs/LeftNav/LeftNav";
@@ -20,30 +20,32 @@ function AppPage() {
 
   return (
     <>
-      <div className="appPage">
-        {isMobile ? <MobileNav /> : <LeftNav />}
-        <div className="main">
-          <Switch>
-            <Route exact path="/">
-              <Newspaper />
-            </Route>
-            <Route path="/attack">Attack</Route>
-            <Route path="/missions">
-              <Tasks />
-            </Route>
-            <Route path="/club">Club </Route>
-            <Route path="/stockmarket">StockMarket</Route>
-            <Route path="/buildings">Buildings</Route>
-            <Route path="/hospital">Hospital</Route>
-            <Route path="/bank">Bank</Route>
-            <Route path="/casino">Casino</Route>
-            <Route path="/prison">Prison</Route>
-            <Route path="*">
-              <Error404 />
-            </Route>
-          </Switch>
+      <div className="app">
+        <div className="appPage">
+          {isMobile ? <MobileNav /> : <LeftNav />}
+          <div className="main">
+            <Switch>
+              <Route exact path="/">
+                <Newspaper />
+              </Route>
+              <Route path="/attack">Attack</Route>
+              <Route path="/missions">
+                <Tasks />
+              </Route>
+              <Route path="/club">Club </Route>
+              <Route path="/stockmarket">StockMarket</Route>
+              <Route path="/buildings">Buildings</Route>
+              <Route path="/hospital">Hospital</Route>
+              <Route path="/bank">Bank</Route>
+              <Route path="/casino">Casino</Route>
+              <Route path="/prison">Prison</Route>
+              <Route path="*">
+                <Error404 />
+              </Route>
+            </Switch>
+          </div>
+          {isMobile ? null : <RightNav />}
         </div>
-        {isMobile ? null : <RightNav />}
       </div>
     </>
   );
