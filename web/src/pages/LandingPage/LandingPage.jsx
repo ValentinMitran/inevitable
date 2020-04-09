d514eba737ef5d334f6370a8b36cb15e510125ec
Valentin Mitran <ToolsVM@pm.me>
2020-04-09 02:36:49 +0100

    Landing Page

--- web/src/pages/LandingPage/LandingPage.jsx
+++ web/src/pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import './LandingPage.scss';
+import React from "react";
+import "./LandingPage.scss";
 
-function LandingPage(){
-    return(<>Landing Page</>);
+function LandingPage() {
+  return (
+    <div id="lp">
+      <div id="lpWrapper">
+        <h1 class="glitch" data-text="Inevitable">
+          Inevitable
+        </h1>
+        <span class="sub">The Cyberpunk era starts here</span>
+        <button class="btn">
+          <span class="btn__content">ENTER</span>
+          <span class="btn__glitch"></span>
+          <span class="btn__label">Inevitable</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
